be59f2e0215e680c6e98a507ce70353cd3a47ece
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-07-19 16:05:20 +0900

    Update 1d_int_array.c

--- Course_array/1d_int_array.c
+++ Course_array/1d_int_array.c
@@ -2,17 +2,14 @@
 
 int main()
 {
-    float weight[10];
-    // total은 누적 연산을 위해 선언과 동시에 0으로 초기화
-    float total = 0, avg;  
+    int num[10];
     int i;
 
-    printf("10명의 몸무게를 입력하시오.\n");
-    for (i=0; i<10; i++){
-        scanf("%f", &weight[i]);
-        total += weight[i];
-    }
-    avg = total / 10;
-    printf("평균 몸무게: %.2f", avg);
+    printf("10개의 정수를 입력하시오.\n");
+    for (i=0; i<10; i++)
+        scanf("%d", &num[i]);
+    for (i=0; i<10; i++)
+        printf("%d. %d\n", i+1, num[i]);
+    
     return 0;
 }
